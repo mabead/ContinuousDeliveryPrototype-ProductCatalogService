7528f680a1bb970fd2fe95d798081fdc5f52377f
Maxime Beaudry <beaudry.maxime@gmail.com>
2016-08-31 05:49:00 -0400

    Add Product model.

---
+++ src/ProductCatalogService/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductCatalogService.Models
+{
+    public class Product
+    {
+        public string Sku { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+    }
+}
