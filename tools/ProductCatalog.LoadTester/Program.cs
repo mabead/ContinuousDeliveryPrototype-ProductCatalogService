6af34c130067227a5ad10023cd411c750b273003
Maxime Beaudry <beaudry.maxime@gmail.com>
2016-09-13 21:08:10 -0400

    Create basic guts of LoadTester

--- tools/ProductCatalog.LoadTester/Program.cs
+++ tools/ProductCatalog.LoadTester/Program.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using ProductCatalog.LoadTester.Generated;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ProductCatalog.LoadTester
@@ -9,6 +12,34 @@ namespace ProductCatalog.LoadTester
     {
         public static void Main(string[] args)
         {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("ERROR: usage ProductCatalog.LoadTester <service_url>");
+                Environment.Exit(1);
+            }
+
+            var serviceUrl = new Uri(args[0]);
+            DoProgram(serviceUrl).Wait();
+        }
+
+        private static async Task DoProgram(Uri serviceUrl)
+        {
+            using (var client = new CatalogServiceAPI(serviceUrl))
+            {
+                var stopWatch = Stopwatch.StartNew();
+
+                for (int i = 1; i <= 1000000; i++)
+                {
+                    var products = await client.ApiProductsGetAsync();                    
+
+                    if (i % 1000 == 0)
+                    {
+                        var millisecondsPerCall = stopWatch.Elapsed.TotalMilliseconds / i;
+
+                        Console.WriteLine($"Done {i} calls in {(int)stopWatch.Elapsed.TotalSeconds} seconds => {millisecondsPerCall} ms/call");
+                    }
+                }
+            }
         }
     }
 }
